102ff17425156658ea7e2f921b66c0324e99b7ee
filtercake <fabian@superpursuitmo.de>
2014-02-08 19:19:43 +0100

    init

---
+++ app.js
@@ -0,0 +1,54 @@
+
+
+
+var tazz = angular.module('tazz', ['angularLocalStorage', 'ngAnimate']);
+ 
+tazz.controller('TazzCtrl', function ($scope, $http, storage) {
+
+  $scope.data = {};
+  $scope.getAppData = function() {
+    $http.get('/data/data.json')
+      .then(function(res){
+        $scope.data = res.data;
+        console.log('yo');
+      });
+  };
+
+  storage.bind($scope,'data');
+
+  $scope.data.addNewThing = function(newThing) {
+    console.log(newThing);
+    $scope.data.myFavouriteThings.push(newThing);
+  };
+  
+  $scope.data.deleteThing = function(thing) {
+    console.log(thing);
+    $scope.data.myFavouriteThings.splice(thing, 1);
+  };
+
+  $scope.world = 'Tazz';
+  $scope.getTazzLibrary = function() {
+    $http.get('/data/tazz.json')
+      .then(function(res){
+        $scope.tazz = res.data;
+        console.log('yo');
+      });
+  };
+  storage.bind($scope,'tazz');
+})
+
+.directive('lib', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/templates/lib.html'
+    };
+})
+
+.directive('app', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/app.html'
+    };
+})
---
+++ bower_components/rocketcss/README.md
@@ -0,0 +1,16 @@
+RocketCSS
+=========
+
+Build your website with RocketCSS, a powerful CSS UI toolkit.
+
+Visit our website at: <a href="http://rocketcss.com">RocketCSS.com</a>
+
+RocketCSS is a very simple and easy to use CSS framework. We came up with the idea of RocketCSS when other CSS frameworks fell short of our expectations. We wanted to show you how easy and useful it can be when using our CSS framework.
+
+From website buttons to website icons, we have it all, our simple, smart, and elegant CSS framework make your website visually exciting and compel visitors to stay and interact.
+
+Our team of astronauts constantly update RocketCSS based on your feedback. In the future, we wish to expand our team and make our CSS framework more responsive to calibrate with all devices. Our CSS framework utilize BEM syntaxes within our code markup, this makes it easier for developers to use.
+
+Contact:<br/>
+Email: info@rocketcss.com
+
